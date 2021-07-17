7db06fe832349fc8c40eb78e3df0de0c858e0985
anhtudo97 <anhtudo97@gmail.com>
2021-07-17 16:28:30 +0700

    feat: add config

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-// const authRoute = require("./routes/auth");
-// const userRoute = require("./routes/users");
-// const postRoute = require("./routes/posts");
-// const categoryRoute = require("./routes/categories");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
+const cors = require("cors");
 
 dotenv.config();
+app.use(cors());
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
@@ -37,9 +39,10 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
     res.status(200).json("File has been uploaded");
 });
 
-app.get("/", (req, res) => {
-    res.send("ok");
-});
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
     console.log("Backend is running.");
